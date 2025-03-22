c123ff0a8b8b09d82bf80de4ba44898b7ef19e54
Arturo Alcalá <alcala28043@gmail.com>
2025-03-22 21:36:34 +0100

    Inicialización del proyecto backend con Spring

---
+++ src/main/java/dev/kpucha/neuron/forge/NeuronForgeApplication.java
@@ -0,0 +1,13 @@
+package dev.kpucha.neuron.forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuronForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kpucha/neuron/forge/NeuronForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kpucha.neuron.forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuronForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
